965269fabe122787f2b229583d2cfab311de31bd
= <7rishi20ss@gmail.com>
2025-02-15 14:53:04 -0800

    add: multiple criteria

--- quiz_class.py
+++ quiz_class.py
@@ -17,17 +17,30 @@ class QuizHandler:
 
         with st.form("quiz_form"):
             responses = {
-                "ideal_date": st.selectbox(
+                "relationship_type": st.multiselect(
+                    "What are you looking for?",
+                    ["Long-Term relationship", "something casual", "something special", "Not sure yet"],
+                    key="quiz_relationship_type"
+                ),
+                "ideal_date": st.multiselect(
                     "What's your ideal first date?",
                     ["Dinner and a movie", "Outdoor adventure", "Coffee and deep conversation", "Fun activity like bowling or mini-golf"]
                 ),
-                "love_language": st.selectbox(
+                "love_language": st.multiselect(
                     "What's your love language?",
                     ["Words of Affirmation", "Acts of Service", "Receiving Gifts", "Quality Time", "Physical Touch"]
                 ),
-                "communication_style": st.radio(
+                "communication_style": st.multiselect(
                     "How do you prefer to communicate in a relationship?",
                     ["Frequent texts and calls", "Quality time in person", "Thoughtful gestures", "Give each other space"]
+                ),
+                "diet_preferences": st.multiselect(
+                    "What are your diet preferences?",
+                    ["Everything", "Vegetarian", "Vegan", "Keto", "Gluten-free", "Halal", "Sattvic"]
+                ),
+                "drink_preferences": st.multiselect(
+                    "What are your drink preferences?",
+                    ["Cocktails", "Wines", "Beers", "Spirits", "Non-alcoholic"]
                 ),
                 "hobbies": st.text_area("What are your favorite hobbies and interests?"),
                 "gift_preference": st.radio(
